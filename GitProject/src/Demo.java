61ad963a90d181c6ac7b198c8ebc6f2613841af5
rahulpahune123 <92918992+rahulpahune123@users.noreply.github.com>
2021-10-21 16:02:54 +0530

    Update Demo.java

--- GitProject/src/Demo.java
+++ GitProject/src/Demo.java
@@ -4,6 +4,11 @@ public class Demo {
 	public void m1() {
 		
 	}
+	public void m2() {
+		
+	}
+	
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
    System.out.println("Hello Git");
